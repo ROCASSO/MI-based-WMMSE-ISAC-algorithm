54113ceaf9f9faccb516393ff6855cf38cd0932f
ROCASSO <105794554+ROCASSO@users.noreply.github.com>
2024-05-13 00:15:08 +0800

    Update runme.m

--- runme.m
+++ runme.m
@@ -2,7 +2,7 @@
 % Communications: A WMMSE Framework
 % Yizhou Peng, Songjie Yang, Wanting Lyu, Ya Li, Hongjun He,
 % Zhongpei Zhang, Member, IEEE, and Chadi Assi, Fellow, IEEE
-%randomly chosen your seed
+%randomly chosen your seed we chose 891236
 sample_num = 300;
 x_axis_all = 0:0.01:0.99;
 rate_all = zeros(100,1,sample_num);
